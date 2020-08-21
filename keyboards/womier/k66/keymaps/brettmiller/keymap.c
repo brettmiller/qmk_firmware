87ee78ca91983ce4de350efd43f549ba7f5f3cec
Brett Miller <github@shadowed.net>
2020-08-20 23:01:36 -0500

    fix arrows on layer 2

--- keyboards/womier/k66/keymaps/brettmiller/keymap.c
+++ keyboards/womier/k66/keymaps/brettmiller/keymap.c
@@ -12,20 +12,20 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TAB         , KC_Q    , KC_W    , KC_E    , KC_R    , KC_T     , KC_Y     , KC_U    , KC_I    , KC_O    , KC_P    , KC_LBRC , KC_RBRC , KC_BSLS , KC_DEL ,
     LCTL_T(KC_ESC) , KC_A    , KC_S    , KC_D    , KC_F    , KC_G     , KC_H     , KC_J    , KC_K    , KC_L    , KC_SCLN , KC_QUOT , KC_ENT  ,
     KC_LSFT        , KC_Z    , KC_X    , KC_C    , KC_V    , KC_B     , KC_N     , KC_M    , KC_COMM , KC_DOT  , KC_SLSH , KC_RSFT , KC_UP   ,
-    MO(2)          , KC_LALT , KC_LGUI ,                     KC_SPC   ,                      KC_RGUI , MO(1)   , KC_RALT , KC_LEFT , KC_DOWN , KC_RGHT
+    MO(1)          , KC_LALT , KC_LGUI ,                     KC_SPC   ,                      KC_RGUI , MO(2)   , KC_RALT , KC_LEFT , KC_DOWN , KC_RGHT
     ),
   [1] = LAYOUT(
     KC_GRV         , KC_F1   , KC_F2   , KC_F3   , KC_F4   , KC_F5    , KC_F6    , KC_F7   , KC_F8   , KC_F9   , KC_F10  , KC_F11  , KC_F12  , KC_MUTE , KC_VOLU ,
     _______        , _______ , _______ , _______ , _______ , _______  , _______  , _______ , KC_PSCR , KC_SLCK , KC_PAUS , _______ , _______ , _______ , KC_VOLD ,
-    MO(1)          , _______ , _______ , _______ , _______ , _______  , _______  , _______ , _______ , _______ , _______ , _______ , _______ ,
+    MO(2)          , _______ , _______ , _______ , _______ , _______  , _______  , _______ , _______ , _______ , _______ , _______ , _______ ,
     _______        , _______ , _______ , _______ , _______ , _______  , _______  , _______ , _______ , _______ , _______ , _______ , KC_PGUP ,
-    MO(2)          , _______ , _______ ,                     _______  ,                      _______ , MO(2)   , MO(1)   , KC_HOME , KC_PGDN , KC_END
+    MO(1)          , _______ , _______ ,                     _______  ,                      _______ , MO(2)   , MO(1)   , KC_HOME , KC_PGDN , KC_END
     ),
   [2] = LAYOUT(
     RGB_MOD       , RGB_RMOD , RGB_M_P , RGB_M_B , RGB_M_R , RGB_M_SW , RGB_M_SN , RGB_M_K , RGB_M_X , RGB_M_G , RGB_M_T , KC_F11  , KC_F12  , RGB_TOG , RGB_VAI ,
     _______       , _______  , _______ , _______ , RESET   , _______  , _______  , _______ , _______ , _______ , _______ , _______ , _______ , _______ , RGB_VAD ,
-    MO(1)         , _______  , _______ , _______ , _______ , _______  , _______  , KC_LEFT , KC_DOWN , KC_UP   , KC_RIGHT , _______ , _______ ,
+    MO(2)         , _______  , _______ , _______ , _______ , _______  , KC_LEFT  , KC_DOWN , KC_UP   , KC_RIGHT, _______ , _______ , _______ ,
     _______       , _______  , _______ , _______ , _______ , _______  , _______  , _______ , _______ , _______ , _______ , _______ , RGB_SAI ,
-    MO(2)         , _______  , _______ ,                     _______  ,                      _______ , MO(2)   , MO(1)   , RGB_HUD , RGB_SAD , RGB_HUI
+    MO(1)         , _______  , _______ ,                     _______  ,                      _______ , MO(2)   , MO(1)   , RGB_HUD , RGB_SAD , RGB_HUI
     )
 };
